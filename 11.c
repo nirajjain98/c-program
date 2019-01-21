8e91695b71c775563bbac1f8a796a67bfd73bd65
nirajjain98 <45127200+nirajjain98@users.noreply.github.com>
2019-01-21 10:50:12 +0530

    Add files via upload

---
+++ 11.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a[50];
+    int i,v=0,c=0;
+    printf("enter string \n");
+    gets(a);
+    for(i=0;i<strlen(a);i++)
+    {
+        if(a[i]=='a'||a[i]=='i'||a[i]=='e'||a[i]=='0'||a[i]=='u')
+        {
+            v++;
+        }
+        else
+        {
+            c++;
+        }
+    }
+    printf("no of vowels are  %d",v);
+    printf("no of consonent %d",c);
+    return(0);
+}
